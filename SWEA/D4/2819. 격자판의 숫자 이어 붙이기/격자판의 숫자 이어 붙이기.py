569b5775f70133a7a8c97a70d4d55b08054f69f6
journnie <155598826+journnie@users.noreply.github.com>
2024-02-16 16:58:06 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 373 ms, Memory: 70,692 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,14 +1,14 @@
-#import sys
-#sys.stdin = open("practice_2.txt", "r")
+# import sys
+# sys.stdin = open("practice_2.txt", "r")
 
 T = int(input())
 for tc in range(1, T + 1):
     arr = [input().split() for _ in range(4)]
     answer = set()
     # move
-    moves = [[-1,0], [0,1], [1,0], [0,-1]] # up, right, down,  left
+    moves = [[-1,0], [0,1], [1,0], [0,-1]]  # up, right, down,  left
 
-    def dfs(answer, position, mv_cnt, number):
+    def dfs(position, mv_cnt, number):
         if mv_cnt == 0:
             answer.add(number)
             return
@@ -21,12 +21,12 @@ for tc in range(1, T + 1):
             if next_x < 0 or next_x >= 4: continue
             if next_y < 0 or next_y >= 4: continue
 
-            dfs(answer, [next_x, next_y], mv_cnt - 1, number+str(arr[next_x][next_y]))
+            dfs([next_x, next_y], mv_cnt - 1, number+str(arr[next_x][next_y]))
 
 
     for i in range(len(arr)):
         for j in range(len(arr[i])):
             number = str(arr[i][j])
-            dfs(answer, [i, j], 6, number)
+            dfs([i, j], 6, number)
 
     print(f'#{tc} {len(answer)}')
