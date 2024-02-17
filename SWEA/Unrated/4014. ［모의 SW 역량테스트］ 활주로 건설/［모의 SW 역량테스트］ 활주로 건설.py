b7cf8548d2eecedb8c9192fce10c32f894d6d3c4
journnie <155598826+journnie@users.noreply.github.com>
2024-02-17 23:46:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 212 ms, Memory: 63,300 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.py
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.py
@@ -9,28 +9,26 @@ def makeRunway(row):
         if row[i] - row[i-1] == 0:
             land += 1
         elif row[i] - row[i - 1] == 1:  # up
-            if down and land < 2*X:  # fig 10
+            if down and land < 2*X: 
                 return False
-            if not down and land < X:  # fig 4
+            if not down and land < X: 
                 return False
             down = False
             land = 1
 
         elif row[i] - row[i-1] == -1:  # down
-            if down and land < X:  # fig 6
+            if down and land < X: 
                 return False
             down = True
             land = 1
-        # 근데 여기까지 안 내려옴..
         else:  # 만약 층이 두칸 이상일 경우 등등
             return False
-    if down and land < X:  # fig 7을 생각하고 쓴 것
+    if down and land < X:  
         return False
     return True
 
 
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     N, X = list(map(int, input().split()))
     arr = [list(map(int, input().split())) for _ in range(N)]
