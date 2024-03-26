025b32d979c5fa9ef544d39a71a0be0621618a47
journnie <155598826+journnie@users.noreply.github.com>
2024-03-26 23:39:13 +0900

    [Silver IV] Title: 괄호, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,24 @@
+T = int(input())
+test_cases = [list(map(str, input())) for _ in range(T)]
+
+
+# T, test_cases = read_input()
+
+for a in test_cases:
+    FLAG = True
+    stack = []
+    for char in a:
+        if char == '(':
+            stack.append(char)
+        else:
+            if not stack:
+                print('NO')
+                FLAG = False
+                break
+            stack.pop()
+    if not FLAG:
+        continue
+    if stack:
+        print('NO')
+    else:
+        print('YES')
