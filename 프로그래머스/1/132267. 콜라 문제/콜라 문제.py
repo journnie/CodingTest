dc0f72dd8d8a96deef74300fe9980a42190ae9bf
journnie <155598826+journnie@users.noreply.github.com>
2024-06-28 16:44:11 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,10 +1,6 @@
 def solution(a, b, n):
     answer = 0
-    empty = n
-
-    while empty >= a:
-        new = empty // a * b
-        left = empty % a
-        empty = new + left
-        answer += new
+    while n >= a:
+        n -= (a - b)
+        answer += b
     return answer
