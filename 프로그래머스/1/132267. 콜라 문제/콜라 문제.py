c21332025a0c4a65d9f3a6c26120ac5ddf54128d
journnie <155598826+journnie@users.noreply.github.com>
2024-06-28 16:39:04 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,10 +1,10 @@
 def solution(a, b, n):
     answer = 0
-    bottle = n
-    while bottle >= a:
-        giving_set = bottle // a
-        get = b * giving_set
-        bottle = bottle % a + get
-        answer += get
-        print(giving_set, get, bottle, answer)
+    empty = n
+
+    while empty >= a:
+        new = empty // a * b
+        left = empty % a
+        empty = new + left
+        answer += new
     return answer
