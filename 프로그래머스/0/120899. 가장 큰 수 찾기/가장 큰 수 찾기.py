1e821c5fdccf10db1a348957e51643b824dd0d73
journnie <155598826+journnie@users.noreply.github.com>
2024-06-28 15:25:08 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,8 +1,4 @@
 def solution(array):
-    # 아래에 코드를 작성해주세요.
-    answer = [array[0], 0]
-    for idx, num in enumerate(array):
-        if num >= answer[0]:
-            answer[0] = num
-            answer[1] = idx
-    return answer
+    num = max(array)
+    idx = array.index(num)
+    return [num, idx]
