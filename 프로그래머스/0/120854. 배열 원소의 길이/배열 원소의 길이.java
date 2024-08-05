f50458e74b280aec57d3fa9aed23ad5fbd8ab1c9
journnie <155598826+journnie@users.noreply.github.com>
2024-08-06 08:58:04 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] lengths = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++) {
+            lengths[i] = strlist[i].length();
+        }
+        return lengths;
+    }
+}
