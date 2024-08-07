93a31852a6b57ee4a260842770a959bd66e0dad5
journnie <155598826+journnie@users.noreply.github.com>
2024-08-07 23:28:21 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int count = 0; 
+        for (int num : array) {
+            if (num > height) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
