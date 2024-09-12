bc0538e9a971865cbc5b0ca380e477527fc0d6c5
journnie <155598826+journnie@users.noreply.github.com>
2024-09-12 22:04:47 +0900

    [D3] Title: 햄버거 다이어트, Time: 162 ms, Memory: 20,880 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,50 +1,43 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
-    static int ingredientNum, limitCalories;
-    static int[] scores;
-    static int[] calories;
-    static int bestHamburger;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            ingredientNum = Integer.parseInt(st.nextToken());
-            limitCalories = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        for(int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int ingredientCount = Integer.parseInt(st.nextToken());
+            int limitCalories = Integer.parseInt(st.nextToken());
+            int[][] ingredients = new int[ingredientCount][2];
 
-            scores = new int[ingredientNum];
-            calories = new int[ingredientNum];
-
-            for (int i = 0; i < ingredientNum; i++) {
+            for(int i = 0; i < ingredientCount; i++) {
                 st = new StringTokenizer(br.readLine());
-                scores[i] = Integer.parseInt(st.nextToken());
-                calories[i] = Integer.parseInt(st.nextToken());
+                ingredients[i][0] = Integer.parseInt(st.nextToken());
+                ingredients[i][1] = Integer.parseInt(st.nextToken());
             }
 
-            bestHamburger = 0;  // bestHamburger 초기화
-            findBestHamburger(0, 0, 0);
-            System.out.printf("#%d %d\n", t, bestHamburger);
+            int[] dp = new int[limitCalories+1];
+            int maxScore = Integer.MIN_VALUE;
+
+
+            for(int i = 0; i < ingredientCount; i++) {
+                for(int calories = limitCalories; calories > 0; calories--) {
+                    int currentScore = ingredients[i][0];
+                    int currentCalories = ingredients[i][1];
+                    // 제한 칼로리보다 높으면 패스
+                    if(currentCalories > calories) continue;
+                    dp[calories] = Math.max(dp[calories], dp[calories - currentCalories]+currentScore);
+                    maxScore = Math.max(maxScore, dp[calories]);
+                }
+            }
+            System.out.printf("#%d %d\n", t, maxScore);
+
+
+
         }
-    }
 
-    static void findBestHamburger(int index, int currentHamburger, int currentCalories) {
-        if (currentCalories > limitCalories) {
-            return;  // 칼로리 제한 초과 시 종료
-        }
-
-        if (index == ingredientNum) {
-            bestHamburger = Math.max(bestHamburger, currentHamburger);
-            return;
-        }
-
-        // 현재 재료를 선택하지 않는 경우
-        findBestHamburger(index + 1, currentHamburger, currentCalories);
-
-        // 현재 재료를 선택하는 경우
-        findBestHamburger(index + 1, currentHamburger + scores[index], currentCalories + calories[index]);
     }
 }
