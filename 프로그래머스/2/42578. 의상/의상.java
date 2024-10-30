ffe6f1a4697a5caaa431f677e1a931ca32b9fb10
journnie <155598826+journnie@users.noreply.github.com>
2024-10-30 15:00:58 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,18 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
-        for (String[] cloth : clothes) {
-            String type = cloth[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
-        }
-
-        int answer = 1; 
-        for (int val : map.values()) {
-            answer *= (val + 1); 
-        }
-
-        return answer - 1;
+        int answer = 0;
+        return answer;
     }
 }
