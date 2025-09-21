9d85a2ac32c4c7d1353771b6356af780c5e5211b
journnie <155598826+journnie@users.noreply.github.com>
2025-09-21 23:49:54 +0900

    [level 1] Title: 모의고사, Time: 5.12 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] students = {{1, 2, 3, 4, 5},
+        int[][] students = {
+                            {1, 2, 3, 4, 5},
                             {2, 1, 2, 3, 2, 4, 2, 5},
-                            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-        
+                            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+                        };
         int[] scores = new int[3];
         
-        for(int s=0; s<students.length; s++) {
-            int[] student = students[s];
-            for(int i=0; i<answers.length; i++){
-                if(answers[i] == student[i%student.length]) scores[s]++;
+        for(int i = 0; i < answers.length; i++) {
+            for(int s = 0; s < students.length; s++){
+                if(answers[i] == students[s][i%students[s].length]){
+                    scores[s]++;
+                }
             }
         }
         
         int maxScore = Arrays.stream(scores).max().getAsInt();
         
-        List<Integer> answer = new ArrayList<>();
+        ArrayList<Integer> topStudent = new ArrayList<>();
         
-        for(int s=0; s<scores.length; s++) {
-            if(scores[s] == maxScore) answer.add(s+1);
+        for (int s = 0; s < scores.length; s++) {
+            if(scores[s] == maxScore) topStudent.add(s+1);
         }
-        
-        return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return topStudent.stream().mapToInt(Integer::intValue).toArray();
     }
 }
