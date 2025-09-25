a4678b19aaec764f6255b39647112dcded8b74da
journnie <155598826+journnie@users.noreply.github.com>
2025-09-25 18:34:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 59.56 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        if(s.length() % 2 == 1) return 0;
+
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++ ){
+            char ch = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek() == ch) stack.pop();
+            else stack.push(ch);
+        }
+
+        return stack.isEmpty()? 1 : 0;
+    }
+}
