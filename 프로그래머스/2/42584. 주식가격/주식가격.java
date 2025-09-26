b54dc5df7fef05bbd2378cba95090ccfbea2e8a2
journnie <155598826+journnie@users.noreply.github.com>
2025-09-27 00:01:11 +0900

    [level 2] Title: 주식가격, Time: 25.29 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int time = 0;
-            for (int j = i + 1; j < n; j++) {
-                time++;
-                if (prices[j] < prices[i]) break;
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i < prices.length; i++){
+            for(int j = i+1; j < prices.length; j++){
+                answer[i]++;
+                if(prices[j] < prices[i]) break;
             }
-            answer[i] = time;
         }
         return answer;
     }
