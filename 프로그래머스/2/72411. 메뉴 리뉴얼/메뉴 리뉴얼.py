215604e9f18b0f3839d992d131ed71a6c0c6fd1a
journnie <155598826+journnie@users.noreply.github.com>
2025-10-08 19:16:00 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 106.26 ms, Memory: 9.55 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    
+    for c in course:
+        menu = []
+        
+        for order in orders:
+            comb = combinations(sorted(order), c)
+            menu += comb
+            
+        counter = Counter(menu)
+
+        if(counter and max(counter.values()) > 1):
+            for m, cnt in counter.items():
+                if cnt == max(counter.values()):
+                    answer.append("".join(m))
+    
+    
+    
+    return sorted(answer)
