a021cd909f27c3bccb90e54e729bd9616345253a
journnie <155598826+journnie@users.noreply.github.com>
2025-10-08 13:07:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -4,16 +4,13 @@ def solution(genres, plays):
     answer = []
     genre_dict = defaultdict(list) # 장르 : [i, plays]
     play_dict = defaultdict(int) # 장르 : 장르총재생수
-    # [장르, 장르 곡 수, 재생횟수]
-    songs = [[] for _ in range(len(genres))]
-    for i, genre in enumerate(genres):
-        play_dict[genre] += plays[i]
-        genre_dict[genre].append((i, plays[i]))
-        
-    sorted_genres = sorted(play_dict.items(), key = lambda x: x[1], reverse=True)
-    
-    for genre, _ in sorted_genres:
-        sorted_songs = sorted(genre_dict[genre], key=lambda x:(-x[1], x[0]))
-        answer.extend([idx for idx, _ in sorted_songs[:2]])
-        
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        genre_dict[g].append((i, p))
+        play_dict[g] += p
+
+    for (k, v) in sorted(play_dict.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(genre_dict[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
+
     return answer
